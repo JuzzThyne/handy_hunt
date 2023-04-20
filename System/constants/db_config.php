fb4b42259b72e0e6ac9ea295fd69a406a87e7b3f
Josh <villanuevajoshua27@gmail.com>
2023-04-20 11:52:30 +0800

    email verification files

--- System/constants/db_config.php
+++ System/constants/db_config.php
@@ -3,4 +3,13 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "job_portal";
+
+// $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// if (!$conn) {
+//     die("<script>alert('Connection Failed.')</script>");
+// } else {
+//     // echo 'Connected madafakaz';
+
+// }
 ?>
